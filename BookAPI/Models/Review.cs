fd24de1382ccfe8730642e07356779eabdaf97f4
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-09 20:28:53 +0300

    create the needed models

---
+++ BookAPI/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookAPI.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Headline { get; set; }
+        public string ReviewText { get; set; }
+        public int Rating { get; set; }
+    }
+}
---
+++ BookAPI/Models/Reviewer.cs
@@ -0,0 +1,9 @@
+﻿namespace BookAPI.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+    }
+}
