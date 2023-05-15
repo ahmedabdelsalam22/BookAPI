8717208fbcdf18ca4e40c80a01572948a9f7a21e
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-15 11:45:14 +0300

    apply AutoMapping

--- BookAPI/Controllers/CountriesController.cs
+++ BookAPI/Controllers/CountriesController.cs
@@ -1,4 +1,6 @@
-﻿using BookAPI.Repository.IRepository;
+﻿using AutoMapper;
+using BookAPI.Dtos;
+using BookAPI.Repository.IRepository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,21 +11,26 @@ namespace BookAPI.Controllers
     public class CountriesController : ControllerBase
     {
         private readonly ICountryRepository countryRepository;
+        private readonly IMapper mapper;
 
-        public CountriesController(ICountryRepository countryRepository)
+        public CountriesController(ICountryRepository countryRepository, IMapper mapper)
         {
             this.countryRepository = countryRepository;
+            this.mapper = mapper;
         }
 
         [HttpGet("allCountries")]
         public IActionResult GetCountries()
         {
             var countries = countryRepository.GetCountries().ToList();
-            if (countries == null) 
+
+            List<CountryDto> countryDTO = mapper.Map<List<CountryDto>>(countries);
+
+            if (countryDTO == null) 
             {
                 return NotFound("No countries found");
             }
-            return Ok(countries);
+            return Ok(countryDTO);
         }
     }
 }
---
+++ BookAPI/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookAPI.Dtos;
+using BookAPI.Models;
+
+namespace BookAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Country,CountryDto>();
+        }
+    }
+}
